0c3e78434653f097b47dd1dde40209fe872c4652
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-10-08 00:54:35 -0300

    feat(confirm-purchase-order): validate user location

--- src/features/confirm-purchase-order/application/confirm-purchase-order.use-case.ts
+++ src/features/confirm-purchase-order/application/confirm-purchase-order.use-case.ts
@@ -1,6 +1,7 @@
 import type { NotifierService } from "@core/notifier/notifier.service";
 import type { AddItemToBePurchasedUseCase } from "@features/add-item-to-be-purchased/application/add-item-to-be-purchased.use-case";
 import { createAddItemToBePurchased } from "@features/add-item-to-be-purchased/domain/add-item-to-be-purchased-input.dto";
+import type { ValidateUserLocationService } from "@features/shared/ports/validate-user-location.service";
 import type { AdjustQuantityDifferenceService } from "./ports/adjust-quantity-difference.service";
 import type { ConfirmPurchaseOrderService } from "./ports/confirm-purchase-order.service";
 import type { GetPendingPurchaseItemsService } from "./ports/get-pending-purchase-items.service";
@@ -11,6 +12,7 @@ export class ConfirmPurchaseOrderUseCase {
   readonly #confirmPurchaseOrderService: ConfirmPurchaseOrderService;
   readonly #addItemToBePurchasedUseCase: AddItemToBePurchasedUseCase;
   readonly #adjustQuantityDifferenceService: AdjustQuantityDifferenceService;
+  readonly #validateUserLocationService: ValidateUserLocationService;
 
   // biome-ignore lint/nursery/useMaxParams: Dependency injection
   constructor(
@@ -18,16 +20,20 @@ export class ConfirmPurchaseOrderUseCase {
     getPendingPurchaseItemsService: GetPendingPurchaseItemsService,
     confirmPurchaseOrderService: ConfirmPurchaseOrderService,
     addItemToBePurchasedUseCase: AddItemToBePurchasedUseCase,
-    adjustQuantityDifferenceService: AdjustQuantityDifferenceService
+    adjustQuantityDifferenceService: AdjustQuantityDifferenceService,
+    validateUserLocationService: ValidateUserLocationService
   ) {
     this.#notifierService = notifierService;
     this.#getPendingPurchaseItemsService = getPendingPurchaseItemsService;
     this.#confirmPurchaseOrderService = confirmPurchaseOrderService;
     this.#addItemToBePurchasedUseCase = addItemToBePurchasedUseCase;
     this.#adjustQuantityDifferenceService = adjustQuantityDifferenceService;
+    this.#validateUserLocationService = validateUserLocationService;
   }
 
   execute(): void {
+    this.#validateUserLocationService.validateUserLocation();
+
     const { isConfirmed } = this.#notifierService.confirm({
       title: "Confirmar orden de compra",
       message: "¿Quieres confirmar la orden de compra?",
--- src/features/confirm-purchase-order/index.ts
+++ src/features/confirm-purchase-order/index.ts
@@ -2,6 +2,8 @@ import { SHEET_ID } from "@core/constants/sheet-id.constant";
 import { notifierService } from "@core/notifier";
 import { getSheetById } from "@core/utils/get-sheet-by-id.util";
 import { addItemToBePurchasedUseCase } from "@features/add-item-to-be-purchased";
+import { ValidateUserLocationSpreadsheetService } from "@features/shared/adapters/validate-user-location-spreadsheet.service";
+import type { ValidateUserLocationService } from "@features/shared/ports/validate-user-location.service";
 import { ConfirmPurchaseOrderUseCase } from "./application/confirm-purchase-order.use-case";
 import type { AdjustQuantityDifferenceService } from "./application/ports/adjust-quantity-difference.service";
 import type { ConfirmPurchaseOrderService } from "./application/ports/confirm-purchase-order.service";
@@ -10,6 +12,7 @@ import { AdjustQuantityDifferenceSheetService } from "./infrastructure/adjust-qu
 import { ConfirmPurchaseOrderSheetService } from "./infrastructure/confirm-purchase-order-sheet.service";
 import { GetPendingPurchaseItemsSheetService } from "./infrastructure/get-pending-purchase-items-sheet.service";
 
+const activeSheet = SpreadsheetApp.getActiveSheet();
 const purchasedOrdersSheet = getSheetById(SHEET_ID.PURCHASED_ORDERS);
 const historicalPurchaseOrdersSheet = getSheetById(SHEET_ID.HISTORICAL_ORDERS);
 const itemsSheet = getSheetById(SHEET_ID.ITEMS);
@@ -23,10 +26,14 @@ const confirmPurchaseOrderService: ConfirmPurchaseOrderService =
 const adjustQuantityDifferenceService: AdjustQuantityDifferenceService =
   new AdjustQuantityDifferenceSheetService(itemsSheet);
 
+const validateUserLocationService: ValidateUserLocationService =
+  new ValidateUserLocationSpreadsheetService(purchasedOrdersSheet, activeSheet);
+
 export const confirmPurchaseOrderUseCase = new ConfirmPurchaseOrderUseCase(
   notifierService,
   getPendingPurchaseItemsService,
   confirmPurchaseOrderService,
   addItemToBePurchasedUseCase,
-  adjustQuantityDifferenceService
+  adjustQuantityDifferenceService,
+  validateUserLocationService
 );
